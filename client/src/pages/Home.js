4a192cc08dd033222bcd34066f525ccfa6f36ee0
ALMA_DEV914 <aungonalna58@gamil.com>
2022-07-11 18:46:23 -0700

    add gallery components

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Navbar from '../components/navbar/Navbar';
 import Header from '../components/header/Header';
+import Gallery from '../components/galleries/Gallery';
+import Footer from '../components/footer/Footer';
 
 function Home() {
   return (
     <div>
       <Navbar/>
       <Header/>
+      <div className="homeContainer">
+        <Gallery/>
+      </div>
+      <Footer/>
     </div>
   )
 }
