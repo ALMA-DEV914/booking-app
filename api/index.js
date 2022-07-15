f593ee42f6b24f417b6e19923b39e1fede101fe6
ALMA_DEV914 <aungonalna58@gamil.com>
2022-07-14 17:23:24 -0700

    deploy

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 const app = express();
 dotenv.config();
@@ -29,6 +30,7 @@ mongoose.connection.on("disconnected", () => {
 app.use(cors())
 app.use(cookieParser())
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
@@ -44,6 +46,10 @@ app.use((err, req, res, next) => {
     message: errorMessage,
     stack: err.stack,
   });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 app.listen(8800, () => {
